db17bddf106a24fb25d87bc36d802f5b691ee875
Tanzina061 <jannattanzina177@gmail.com>
2026-04-15 20:04:50 +0600

    add code

---
+++ compiler design.py
@@ -0,0 +1,21 @@
+import re
+
+code = "int sum = 10; a + b = 20;"
+
+# Token patterns
+tokens = re.findall(r'[a-zA-Z_]+|\d+|[=+;]', code)
+
+# কিছু keyword define
+keywords = ['int', 'float', 'if', 'else', 'return']
+
+for token in tokens:
+    if token in keywords:
+        print(token, "-> Keyword")
+    elif token.isdigit():
+        print(token, "-> Number")
+    elif token in ['=', '+']:
+        print(token, "-> Operator")
+    elif token == ';':
+        print(token, "-> Separator")
+    else:
+        print(token, "-> Identifier")
